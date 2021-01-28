e27dba1742f58f86a237f9e4029f7674408464e6
elvisb155 <77465660+elvisb155@users.noreply.github.com>
2021-01-28 16:20:02 -0600

    util added

---
+++ docs/js/util.js
@@ -0,0 +1,13 @@
+function random(low, high) {
+    let multiplier = 1;
+    let shift = 0;
+  
+    if (low && high) {
+      multiplier = high - low;
+      shift = low;
+    } else if (low && !high) {
+      multiplier = low;
+    }
+    let result = Math.random() * multiplier + shift;
+    return result;
+  }
